f5750278badaa559be08b02523b88cdbc1465c52
akshay-7576 <akshaybhardwaj800016@gmail.com>
2026-06-22 22:43:54 +0530

    assignment

---
+++ Day 18/d183.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a[50], n, key;
+    int low=0, high, mid, i;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    scanf("%d",&key);
+
+    high = n-1;
+
+    while(low<=high) {
+        mid=(low+high)/2;
+
+        if(a[mid]==key) {
+            printf("Element found at position %d", mid+1);
+            return 0;
+        }
+        else if(a[mid]<key)
+            low=mid+1;
+        else
+            high=mid-1;
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
