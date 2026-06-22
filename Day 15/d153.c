4d3a6232c0b88b50bc6b4fe3abb80e3fe6f2d6cf
akshay-7576 <akshaybhardwaj800016@gmail.com>
2026-06-22 22:08:21 +0530

    assignment

---
+++ Day 15/d153.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[100], temp;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    temp = arr[n - 1];
+
+    for(int i = n - 1; i > 0; i--)
+        arr[i] = arr[i - 1];
+
+    arr[0] = temp;
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
