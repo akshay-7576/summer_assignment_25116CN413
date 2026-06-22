87e47853c88f92c4b19a63b9cb3dece87a1bec50
akshay-7576 <akshaybhardwaj800016@gmail.com>
2026-06-22 23:13:32 +0530

    assignment

---
+++ Day 22/d222.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[200];
+    int i, words = 1;
+
+    printf("Enter sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == ' ' && str[i + 1] != ' ')
+            words++;
+    }
+
+    printf("Total Words = %d", words);
+
+    return 0;
+}
