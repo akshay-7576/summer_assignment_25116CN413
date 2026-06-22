208de6c62b9021a4e38ec492f64b68cc8c5a26d1
akshay-7576 <akshaybhardwaj800016@gmail.com>
2026-06-22 22:01:04 +0530

    assignment

---
+++ Day 13/d131.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[100];
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Array elements: ");
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
